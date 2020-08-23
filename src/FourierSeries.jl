71a15d6b2a2020fd00135dd6dbe2d549619a16b1
Christian Kral <dr.christian.kral@gmail.com>
2020-08-23 21:53:56 +0200

    Remove non-conform Markdown documentation Maybe @doc raw""" ... """ could be used https://docs.julialang.org/en/v1/

--- src/FourierSeries.jl
+++ src/FourierSeries.jl
@@ -17,14 +17,6 @@ module FourierSeries
     Calculates the real Fourier coefficients of a step function based on the
     data pairs `t` and `u` according to the following variables
 
-    ```math
-    \mathtt{a[k]} =
-    \frac{2}{\mathtt{T}} \int_{\mathtt{t[1]}}^{\mathtt{t[1]+T}}
-    \mathtt{u(t)} \cos(k\omega \mathtt{t}) \operatorname{d}\mathtt{t} \\
-    \mathtt{b[k]} =
-    \frac{2}{\mathtt{T}} \int_{\mathtt{t[1]}}^{\mathtt{t[1]+T}}
-    \mathtt{u(t)} \sin(k\omega \mathtt{t}) \operatorname{d}\mathtt{t}
-    ```
 
     # Function arguments
 
